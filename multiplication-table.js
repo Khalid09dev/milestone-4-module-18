bc17a9df15cb8b2ecdf7cecf892bcb6d81862b4b
khalid09dev <mdkhalidhossen@hotmail.com>
2024-02-17 15:14:35 +0600

    added multiplication-table.js file that works-- to generate a multiplication table for num 5

---
+++ multiplication-table.js
@@ -0,0 +1,10 @@
+//As ersa is learning now, she wants to explore more and more. Tell ersa to generate a multiplication table for num 5
+let num = 1;
+
+while(num <= 100) {
+    // console.log(num);
+    const multiply = 5 * num;
+    console.log('5','multiplied by',num,':',multiply);
+    console.log();
+    num++;
+}
