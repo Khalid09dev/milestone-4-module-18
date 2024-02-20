edb580c0223f9de9c088b21d0a00f16e0c6c97e1
khalid09dev <mdkhalidhossen@hotmail.com>
2024-02-20 12:49:39 +0600

    added old js tasks through while-loop

---
+++ commitment.js
@@ -0,0 +1,6 @@
+let a = 1;
+
+while(a <= 60) {
+    console.log('I will invest at least 6 hrs every single day for next 60 day!', a);
+    a++;
+}
---
+++ odd-even.js
@@ -0,0 +1,26 @@
+// how to find odd numbers 
+let number = 61;
+
+while(number <= 100) {
+    // console.log(number);
+    
+    if(number % 2 !== 0) {
+        console.log('odd number:',number);
+    }
+    number++;
+}
+
+console.log('*****************************');
+console.log('*****************************');
+
+// how to find even numbers 
+let num = 78;
+
+while(num <= 98) {
+    // console.log(num);
+    
+    if(num % 2 === 0) {
+        console.log('even number:',num)
+    }
+    num++;
+}
