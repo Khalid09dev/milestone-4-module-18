3e8b29f346b3bad7739abb394d10fb6cf292b4e6
khalid09dev <mdkhalidhossen@hotmail.com>
2024-02-20 13:02:42 +0600

    added odd-even2.js file that works to figure out odd and even numbers

---
+++ odd-even2.js
@@ -0,0 +1,13 @@
+for(let i = 61; i <= 100; i++) {
+    if(i % 2 !== 0) {
+        console.log(i, 'is an odd number');
+    }
+}
+
+console.log('-------------------------------------')
+
+for(let i = 78; i <= 98; i++) {
+    if(i % 2 === 0) {
+        console.log(i, 'is an even number');
+    }
+}
