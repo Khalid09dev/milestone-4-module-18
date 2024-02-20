b581001f84b5729620ffb802f1748d10baf82d4b
khalid09dev <mdkhalidhossen@hotmail.com>
2024-02-20 12:46:22 +0600

    This is a new commit for what I originally planned to be amended

--- commitment.js
+++
@@ -1,6 +0,0 @@
-let a = 1;
-
-while(a <= 60) {
-    console.log('I will invest at least 6 hrs every single day for next 60 day!', a);
-    a++;
-}
--- for-commitment.js
+++
@@ -1,3 +0,0 @@
-for(let a = 1; a <= 60; a++) {
-    console.log('I will invest at least 6 hrs everyday for next 60 days!');
-}
